ce2518441ed0812d48b7b71c637ef86a975e8a39
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-03 21:58:28 +0530

    Time: 3 ms (81.43%), Space: 46 MB (99.42%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,33 +1,18 @@
 class Solution {
-    public int findJudge(int n, int[][] t) {
-        int res=0,ju=1;
-      if(t.length==0)
-          return n==1?n:-1;  
-      for(int i=0;i<n;i++){
-       for(int j=0;j<t.length;j++){
-           if(t[j][0]==ju){
-               if(ju==n){
-                  return -1; 
-               }
-               ju++;
-               break;
-           }
-           if(j==t.length-1){
-               res=check(ju,t,n);
-           }
-           
-       }
-      }
-        return res;
-    }
-    public int check(int ju,int[][]t,int n){
-        HashMap<Integer,Integer>h=new HashMap<>();
-        for(int i=0;i<t.length;i++){
-            if(t[i][1]==ju)
-            h.put(t[i][0],t[i][1]);
+    public int findJudge(int n, int[][] trust) {
+        
+        int count[] = new int[n+1];
+        
+        for(int t[] : trust){
+            count[t[0]]--;     // outdegree
+            count[t[1]]++;   // indegree
         }
-        if(h.size()==n-1)
-            return ju;
+        
+        for(int i=1;i<=n;i++){
+            if(count[i]==n-1) return i;
+        }
+        
         return -1;
+        
     }
 }
