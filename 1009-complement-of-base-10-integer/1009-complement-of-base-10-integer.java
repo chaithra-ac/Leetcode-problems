1149d459688eb564e166ec9eef3954ee8187e251
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-04 22:08:26 +0530

    Time: 0 ms (100.00%), Space: 35.8 MB (42.29%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,12 +1,8 @@
-class Solution {
-    public int bitwiseComplement(int num) {
-       String  r="";
-        if(num==0)return 1;
-        while(num>0){
-          int res=((num&1)==1?0:1);
-           r=""+res+r;
-        num= num>>1;
-       }
-        return Integer.parseInt(r,2);
+class Solution{
+public int bitwiseComplement(int n) {
+        int mask=1;
+        while(mask<n)
+            mask=(mask<<1)+1;
+        return mask^n;
     }
 }
