8598a3c614ac6fea35ff8a7aeb4e64ed5c24fd7b
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-11 10:30:24 +0530

    Time: 3 ms (9.15%), Space: 40.6 MB (5.11%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,21 +1,37 @@
-public class Solution {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
-        
-        if(root == null) return wrapList;
-        
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelNum = queue.size();
-            List<Integer> subList = new LinkedList<Integer>();
-            for(int i=0; i<levelNum; i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                subList.add(queue.poll().val);
+       List<List<Integer>>outer=new ArrayList<>();
+        Queue<TreeNode>q=new LinkedList<>();
+        if(root!=null)
+            q.add(root);
+        while(!q.isEmpty()){
+            int s=q.size();
+            outer.add(new ArrayList<>());
+            for(int i=0;i<s;i++){
+                TreeNode cur=q.peek();
+                if(cur.left!=null)
+                    q.add(cur.left);
+                if(cur.right!=null)
+                    q.add(cur.right);
+                outer.get(outer.size()-1).add(q.poll().val);  
             }
-            wrapList.add(subList);
+            
         }
-        return wrapList;
+        return outer;
     }
 }
