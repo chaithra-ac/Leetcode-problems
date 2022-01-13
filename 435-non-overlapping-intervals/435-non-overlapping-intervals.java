28ba3ad66275bbab70362c4c49528b089b4104b4
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-13 08:54:57 +0530

    Time: 1910 ms (5.06%), Space: 123.3 MB (5.39%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,22 +1,19 @@
 class Solution {
-    int res=0;
-    public int eraseOverlapIntervals(int[][] time) {
-     Arrays.sort(time,(int []a,int[]b)->Integer.compare(a[1],b[1]));
-        
-        int end=time[0][1];
-        for(int i[]:time){
-            System.out.println(i[0]+","+i[1]);
-            
-            if(end-i[0]<=0){
-                end=i[1];
+    int res=1;
+    public int findMinArrowShots(int[][] points) {
+       Arrays.sort(points,(a,b)->{
+        return a[1]>b[1]?1:-1;   
+       });
+        int arrow=points[0][1];
+        for(int i[]:points){
+            if(i[0]<=arrow){
                 continue;
             }
             else{
-                System.out.println(i[1]);
+                arrow=i[1];
                 res++;
             }
-            
         }
-        return res-1;
+        return res;
     }
 }
