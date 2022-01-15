bad6a653a73c96210131710aa2d011db4ef74f52
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-15 09:38:50 +0530

    Time: 18 ms (9.18%), Space: 42.1 MB (11.85%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>l=new ArrayList();
+        compute(l,new ArrayList(),candidates,target,0);
+        return l;
+    }
+    void compute(List<List<Integer>>l,List<Integer>cur,int[]a,int t,int n){
+        if(t<0||n==a.length){
+            return;
+        }
+        if(t==0){
+            l.add(new ArrayList(cur));
+            System.out.println(cur);
+            return;
+        }
+        cur.add(a[n]);
+        compute(l,cur,a,t-a[n],n);
+        cur.remove(cur.size()-1);
+        // t=t+a[n];
+        compute(l,cur,a,t,n+1);
+        
+    }
+}
