6ea0a32956f23edb1661d06699e61cd37a177cec
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-15 14:54:01 +0530

    Time: 1 ms (46.71%), Space: 38.3 MB (20.79%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -4,21 +4,19 @@ class Solution {
         compute(k,n,1,new ArrayList());
         return l;
     }
-    public void compute(int k,int n,int ele,List<Integer>cur){
-        if(cur.size()==k&&n==0){
+    public void compute(int k,int remain,int num,List<Integer>cur){
+        if(cur.size()==k&&remain==0){
             l.add(new ArrayList(cur));
             return;
         }
-
-        else if(ele>9||n<0){
+        else if(num>9||remain<0){
             return;
         }
         if(cur.size()<=k){
-            cur.add(ele);
-        compute(k,n-ele,ele+1,cur);
+        cur.add(num);
+        compute(k,remain-num,num+1,cur);
         cur.remove(cur.size()-1);
-        compute(k,n,ele+1,cur);
+        compute(k,remain,num+1,cur);
         }
-        // else return;
     }
 }
