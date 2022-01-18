4dfe5fd604e5d4d70a29284bc552084a56bfa005
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-18 10:17:46 +0530

    Time: 2 ms (31.85%), Space: 49.3 MB (24.30%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -1,27 +1,17 @@
 class Solution {
     public boolean canPlaceFlowers(int[] nums, int n) {
         if(n==0)return true;
+        if(nums.length==1)
+            return (n==1&&nums[0]==0)?true:false; 
      for(int i=0;i<nums.length;i++){
-         System.out.print(i);
-         if(nums[i]==0&&n>0){
-         if(nums.length==1)
-            return (n==1&&nums[i]==0)?true:false; 
-         
-         if(i==0&&nums[i+1]==0){
+        if(nums[i]==0&&n>0){
+              
+         if(i==0&&nums[i+1]==0||
+            i==nums.length-1&&nums[nums.length-2]==0||
+            i>0&&nums[i- 1]==0&&nums[i+1]==0){
              nums[i]=1;
-             n--;continue;
-         }
-         
-         else if(i==nums.length-1&&nums[nums.length-2]==0){
-             nums[i]=1;
-             n--;continue;
-         }
-         
-         else if(i>0&&nums[i-1]==0&&nums[i+1]==0){
-             nums[i]=1;
-             n--;
-         }
-         }  
+             n--;}
+        }
      }
         return n==0;
     }
