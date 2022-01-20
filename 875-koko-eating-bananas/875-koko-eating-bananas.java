cae53401c2e1098a0c59292255200bccf9c5ae4a
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-20 09:37:34 +0530

    Time: 55 ms (5.78%), Space: 52.5 MB (5.33%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        double s=0;
+        int res=0,max=0;
+     for(int i:piles){
+         s+=i;
+         if(max<i)max=i;
+     }
+        // System.out.print(s>Integer.MAX_VALUE);
+        int start=0,end=(int)s,mid=0;
+        if(s>Integer.MAX_VALUE)end=1000000000;
+           res=end;
+        while(start<end){
+            mid=start+(end-start)/2; 
+            // System.out.println(mid);
+            if(res<mid){
+                return res;
+            }
+            if(canComplete(piles,mid,h)&&mid<=res){
+               end=mid;
+            // System.out.println(mid);
+                res=mid;
+            }
+            else if(!canComplete(piles,mid,h))
+                start=mid+1;;
+        }
+    return res;
+    }
+    boolean canComplete(int[]piles,int mid,int h)
+    {
+        int hr=0;
+        for(int i:piles){
+            hr+=(int)Math.ceil((double)i/mid);
+        }
+        return hr<=h;
+    }
+}
