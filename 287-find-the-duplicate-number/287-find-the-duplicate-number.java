e20fa60483859a8bc123ee8d148292197d8636be
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-23 20:58:17 +0530

    Time: 6 ms (63.81%), Space: 73.4 MB (39.78%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+    for(int i=0;i<nums.length;i++){
+        int n=Math.abs(nums[i]);
+        if(nums[n]<1)return n;
+        nums[n]*=-1;  
+    }
+        return 0;
+    }
+}
