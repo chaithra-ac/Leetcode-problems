3b22c9d4f2b1564664cac1232e752c08984d0ad2
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-27 13:40:38 +0530

    Time: 35 ms (14.78%), Space: 62.6 MB (7.58%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,43 +1,53 @@
 class Solution {
-        List<Integer>l=new ArrayList<>();
+    List<Integer> l = new ArrayList<>();
+
     public List<Integer> findClosestElements(int[] a, int k, int x) {
-            int n=a.length,p=0;
-            if(x>a[0]&&a[n-1]>x){
-           for(int i=0;i<n;i++){
-                   if(a[i]>x){     
-                     p=i-1;break;      
-                   }
-           }
-           }
-                 if(x<=a[0])p=0;
-                   if(a[n-1]<=x)
-                           p=n-1;
-                int f=p+1,c=0,y=0;
-            System.out.print(p);
-             while(k!=0&&p>=0&&f<n){
-                    c=Math.abs(a[p]-x);
-                    y=Math.abs(a[f]-x);
-                     if(c<y||c==y&&a[p]<a[f]){
-                             l.add(a[p--]);
-                             k--;
-                     }
-                     else{
-                             l.add(a[f++]);
-                             k--;
-                             
-                     }
-             }
-             while(k!=0&&p<0){
-                     l.add(a[f++]);
-                     k--;
-                     
-             }
-                while(k!=0&&f>=n){
-                        l.add(a[p--]);
-                                k--;
-                }
-                Collections.sort(l);
-            return l;
-                        
+        int n = a.length, p = 0, c = 0, y = 0, s = 0, e = n;
+//      find the position of the 'x' in the range of the array using simple binary search
+        while (s < e) {
+            p = s + (e - s) / 2;
+            if (a[p] == x) break;
+            else if (a[p] > x) e = p;
+            else s = p + 1;
         }
+//      if the 'x' is not in range of array then the 'p' will return the position of 'x' to be
+//      so, move the position to -1 so that the range towards left and right can be easily maintained
+            
+        p = a[p] == x ? p : s - 1;
+
+
+//      once the position of x is found, find the closest elements for 'x' upto 'k' and add them to list
+        int f = p + 1;
+
+System.out.print(p+" "+f);
+        while (k != 0 && p >= 0 && f < n) {
+
+
+            c = Math.abs(a[p] - x);
+            y = Math.abs(a[f] - x);
+            if (c < y || c == y && a[p] < a[f]) {
+                l.add(a[p--]);
+                k--;
+            } else {
+                l.add(a[f++]);
+                k--;
+
+            }
+        }
+//if in the case at any time while finding closest if we cross one of the side is lost (index out of bounce) but we need upto k so just add remaining elements simply into the list
+
+        while (k != 0 && p < 0) {
+            l.add(a[f++]);
+            k--;
+
+        }
+        while (k != 0 && f >= n) {
+            l.add(a[p--]);
+            k--;
+        }
+//             at last sort the list and return
+        Collections.sort(l);
+        return l;
+
+    }
 }
