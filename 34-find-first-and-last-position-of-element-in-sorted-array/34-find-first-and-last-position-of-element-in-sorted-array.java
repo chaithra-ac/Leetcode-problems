d6a76daa49be0f2ca3a0d3359fffb54bf53f6fbe
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-30 13:07:27 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (7.11%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,60 +1,38 @@
 class Solution {
-    public int[] searchRange(int[] nums, int t) {
-     int s=0,e=nums.length,mid=0;
-            int[]res={-1,-1};
-            while(s<e){
-                    mid=s+(e-s)/2;
-                    if(nums[mid]==t){
-                            res[0]=mid;
-                            res[1]=mid;
-                            // System.out.println(mid);
-                            leftbs(nums,0,mid,res,t);
-                        // System.out.println(res[0]);
-                            rightbs(nums,mid+1,nums.length,res,t);
-                        // System.out.println(res[1]);
-                            return res;
-                            
-                    }
-                    else if(nums[mid]<t)s=mid+1;
-                    else e=mid;
-            }
-      return res;      
-    }
-    void leftbs(int[]nums,int s,int e,int[] res,int t){
-            
+    public int[] searchRange(int[] nums, int target) {
+        int j=0;
+       int []a={-1,-1};
+        int start=0;
+        int end=nums.length;
+        if(nums.length==0){
+            return a;
+        }
         int mid=0;
-            while(s<e){
-                    mid=s+(e-s)/2;
-                    if(mid>0&&nums[mid]==t&&nums[mid-1]<t||
-                       mid==0&&nums[mid]==t){
-                            res[0]=mid;
-                            return;
-            }
-
-                    if(nums[mid]==t)
-                            e=mid;
-                   else if(nums[mid]<t)
-                            s=mid+1;
-                    
-            }
+      while(start<end){
+          if(start==end&&nums[start]!=target){
+              return new int []{-1,-1};
+          }
+        mid=start+(end-start)/2;
+          if(nums[mid]==target){
+               j=mid;
+              while(j>=0&&nums[j]==target){
+                  a[0]=j;
+                  j--;
+              }
+              j=mid;
+              while(j<nums.length&&nums[j]==target){
+                  a[1]=j;
+                  j++;
+              }
+              return a;
+          }    
+            if(nums[mid]>target)
+                 end=mid;
+             
+             else if(nums[mid]<target)
+                  start=mid+1;
+              
+       }
+      return a;  
     }
-        
-    void rightbs(int[]nums,int s,int e,int[]res,int t){
-          int mid=0,n=nums.length-1;
-                        // System.out.println(s+" "+e);
-
-            while(s<e){
-                    mid=s+(e-s)/2;
-                        System.out.println(mid);
-
-        if(mid+1<=n&&nums[mid]==t&&nums[mid+1]>t||mid==n&&nums[mid]==t){
-                res[1]=mid;
-                return;}
-                    else if(nums[mid]==t)
-                            s=mid+1;
-                    else if(nums[mid]>t)
-                            e=mid;
-            }
-    }
-
 }
