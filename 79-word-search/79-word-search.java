ad623b5e0ae12818608534b43e637dc810d5391e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-08 15:36:42 +0530

    Time: 100 ms (81.69%), Space: 40.3 MB (30.50%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean exist(char[][] b, String w) {
-        boolean[][]visited=new boolean[b.length][b[0].length];
+
      for(int i=0;i<b.length;i++){
          for(int j=0;j<b[0].length;j++){
-             if(b[i][j]==w.charAt(0)&&compute(i,j,b,w,visited,0))
+             if(b[i][j]==w.charAt(0)&&compute(i,j,b,w,0))
                  return true;
          }
      }
         return false;
     }
-    boolean compute(int i,int j,char[][]b,String w,boolean[][]visited,int x){
+    boolean compute(int i,int j,char[][]b,String w,int x){
         if(x==w.length()){
             return true;
     }
         
         else if(i>=b.length||i<0||j>=b[0].length||j<0||
-           w.charAt(x)!=b[i][j]||x>w.length()||visited[i][j]==true)
+           w.charAt(x)!=b[i][j]||x>w.length())
             return false;
         
              b[i][j]='+';
-    boolean found=compute(i+1,j,b,w,visited,x+1)||compute(i-1,j,b,w,visited,x+1)||compute(i,j+1,b,w,visited,x+1)||compute(i,j-1,b,w,visited,x+1);
+    boolean found=compute(i+1,j,b,w,x+1)||compute(i-1,j,b,w,x+1)||compute(i,j+1,b,w,x+1)||compute(i,j-1,b,w,x+1);
              b[i][j]=w.charAt(x);
              return found;
     }
