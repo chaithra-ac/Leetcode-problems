b995d92274dfbc6ec6f1557d3984b00725007594
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-08 15:35:20 +0530

    Time: 154 ms (52.78%), Space: 42.7 MB (17.41%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,21 +1,16 @@
 class Solution {
-    int x=0,f=0;
     public boolean exist(char[][] b, String w) {
         boolean[][]visited=new boolean[b.length][b[0].length];
      for(int i=0;i<b.length;i++){
          for(int j=0;j<b[0].length;j++){
-             x=0;f=0;
-             if(b[i][j]==w.charAt(x))
-                 compute(i,j,b,w,visited,x);
-                 if(f==1)
+             if(b[i][j]==w.charAt(0)&&compute(i,j,b,w,visited,0))
                  return true;
-     }
+         }
      }
         return false;
     }
     boolean compute(int i,int j,char[][]b,String w,boolean[][]visited,int x){
         if(x==w.length()){
-            f=1;
             return true;
     }
         
@@ -23,12 +18,9 @@ class Solution {
            w.charAt(x)!=b[i][j]||x>w.length()||visited[i][j]==true)
             return false;
         
-         else if(w.charAt(x)==b[i][j]){
              b[i][j]='+';
     boolean found=compute(i+1,j,b,w,visited,x+1)||compute(i-1,j,b,w,visited,x+1)||compute(i,j+1,b,w,visited,x+1)||compute(i,j-1,b,w,visited,x+1);
              b[i][j]=w.charAt(x);
              return found;
-        }
-        return false;
     }
 }
