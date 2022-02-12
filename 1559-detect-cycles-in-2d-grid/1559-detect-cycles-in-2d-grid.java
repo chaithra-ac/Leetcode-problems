8b0de693fb89ba84a44006836c880f75a13d3c31
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-12 12:04:24 +0530

    Time: 34 ms (47.27%), Space: 162.3 MB (6.54%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean containsCycle(char[][] g) {
+        int m=g.length,n=g[0].length;
+        boolean [][]b=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                List<String>l=new ArrayList<>();
+                if(!b[i][j]&&dfs(g,i,j,m,n,b,g[i][j])){
+                    return true;
+                }
+                
+            }
+        }
+                return false;
+    }
+    boolean dfs(char[][]g,int i,int j,int m,int n,boolean[][]v,char a){
+        if(i>=m||j>=n||i<0||j<0||g[i][j]!=a)
+            return false;
+        
+        else if(v[i][j])
+            return true;
+        
+                
+        v[i][j]=true;
+        char dummy=g[i][j];
+         g[i][j]='*';
+        boolean has=
+            dfs(g,i+1,j,m,n,v,a)||
+            dfs(g,i-1,j,m,n,v,a)||
+            dfs(g,i,j+1,m,n,v,a)||
+            dfs(g,i,j-1,m,n,v,a);
+        g[i][j]=dummy;
+        return has;
+    }
+}
