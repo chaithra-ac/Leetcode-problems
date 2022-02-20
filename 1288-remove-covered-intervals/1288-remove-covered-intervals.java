3ee2956d94fe4e20c105f40fdf74ef15e03ff131
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-20 20:44:59 +0530

    Time: 8 ms (56.63%), Space: 46.2 MB (24.34%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,20 +1,14 @@
 class Solution {
-    public int removeCoveredIntervals(int[][] nums) {
-        int i=0,res=nums.length;
-     while(i<nums.length){
-         for(int[]j:nums){
-             if(!(nums[i][0]==j[0]&&nums[i][1]==j[1])&&nums[i][0]<=j[0]&&nums[i][1]>=j[1]){
-                 // System.out.println(nums[i][0]+" "+nums[i][1]+" "+j[0]+" "+j[1]);
-                 res--;
-                 j[0]=j[1]=-1;
-             }
-         }
-         // for(int[]check:nums)
-             // System.out.println(check[0]+" "+check[1]);
-         i++;
-     }
-        return res;
-             
-   
+        public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (i, j) -> (i[0] == j[0] ? j[1] - i[1] : i[0] - j[0]));
+        int count = 0, cur = 0;
+        for (int[] a : intervals) {
+            if (cur < a[1]) {
+                cur = a[1];
+                ++count;
+            }
+        }
+        return count;        
     }
+
 }
