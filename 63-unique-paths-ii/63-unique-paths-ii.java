f447e02b5d84c919d9ec667d423def650d5d009d
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-23 12:33:42 +0530

    Time: 40 ms (5.08%), Space: 43.8 MB (5.69%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    int max=Integer.MAX_VALUE;
+    public int uniquePathsWithObstacles(int[][] g) {
+        int m=g.length,n=g[0].length;
+        if(g[0][0]==1||g[m-1][n-1]==1)return 0;
+        int one=1;
+        for(int i=0;i<m;i++){
+         if(g[i][0]==0)
+             g[i][0]=one;
+            else{
+                g[i][0]=max;
+                one=max;
+            }    
+        }
+        one=1;
+        for(int j=1;j<n;j++){
+          if(g[0][j]==0)
+              g[0][j]=one;
+              else{
+                  g[0][j]=max;
+                  one=max;
+              } 
+        }
+        
+        
+      for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+                 System.out.print(g[i][j]+" ");    
+          }
+          System.out.println();
+      }
+      for(int i=1;i<m;i++){
+       for(int j=1;j<n;j++){
+           if(g[i][j]==1){
+               g[i][j]=max;
+           }
+           else if(g[i-1][j]==max)
+               g[i][j]=g[i][j-1];
+           else if(g[i][j-1]==max)
+               g[i][j]=g[i-1][j];
+           else
+        g[i][j]=g[i-1][j]+g[i][j-1];
+       }
+      }
+        return g[m-1][n-1]==max?0:g[m-1][n-1];
+    }
+}
