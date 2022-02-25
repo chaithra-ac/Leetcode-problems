4c5f0a81102de066b3bead29ec8b16b5733550aa
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-25 22:13:26 +0530

    Time: 5 ms (56.00%), Space: 57.7 MB (17.89%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,24 +1,30 @@
 class Solution {
-    public int numIslands(char[][] g) {
-        int m=g.length,n=g[0].length,ans=0;
-     boolean[][]b=new boolean[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(g[i][j]=='1'&&b[i][j]==false){
-                    compute(i,j,g,b,m,n);
-                    ans++;
-                }
-            }
-        }
-        return ans;
-    }
-    void compute(int i,int j,char [][]g,boolean[][]b,int m,int n){
-       if(i>=m||i<0||j>=n||j<0||b[i][j]==true||g[i][j]=='0')return;
-        b[i][j]=true;
-        compute(i+1,j,g,b,m,n);
-        compute(i-1,j,g,b,m,n);
-        compute(i,j+1,g,b,m,n);
-        compute(i,j-1,g,b,m,n);
+    boolean[][]vis;
+    int m,n,res;
+    public int numIslands(char[][] grid) {
+       m=grid.length;
+       n=grid[0].length;
+       vis=new boolean[m][n];
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(grid[i][j]=='1'&&(!vis[i][j])){
+                   res++;
+                   dfs(grid,i,j);
+               }
+           }
+       }
+        return res;
         
     }
+        void dfs(char[][]g,int i,int j){
+        if(i<0||j<0||i>=m||j>=n||vis[i][j]||g[i][j]==0)return;
+        if(g[i][j]=='1'){
+        vis[i][j]=true;
+            dfs(g,i+1,j);
+            dfs(g,i,j+1);
+            dfs(g,i-1,j);
+            dfs(g,i,j-1);
+        }
+        }
+
 }
