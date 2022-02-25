7bb3de8fd5204b0615453373fac7febcc49b3884
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-25 21:48:05 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (11.97%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -3,12 +3,8 @@ class Solution {
      int min=salary[0],max=salary[0],sum=0,f=0;
     for(int i=0;i<salary.length;i++){
         sum+=salary[i];
-        if(min>salary[i]){
-             min=salary[i];
-        }
-        else if(max<salary[i]){
-            max=salary[i];
-        }
+        min=Math.min(salary[i],min);
+        max=Math.max(salary[i],max);
     }
         return (sum-min-max)/(salary.length-2.0);
     }
