3dfa2b41313b93026c60790fe2ae23b26a2e2d8b
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-26 21:51:54 +0530

    Time: 1 ms (100.00%), Space: 42.8 MB (79.30%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,19 +1,26 @@
 class Solution{
+    int m,n,cnt;
     public int closedIsland(int[][] grid) {
-        int cnt = 0;
-        for (int i = 1; i < grid.length-1; ++i)
-            for (int j = 1; j < grid[0].length-1; ++j)
+        
+        m=grid.length;
+        n=grid[0].length;
+        
+        for (int i = 1; i <m-1; ++i)
+            for (int j = 1; j < n-1; ++j)
                 if (grid[i][j] == 0)
                     cnt += dfs(i, j, grid);
         return cnt;
     }
     
     private int dfs(int i, int j, int[][] g) {
-        if (i < 0 || i >= g.length || j < 0 || j >= g[0].length)
+        if (i < 0 || i >=m || j < 0 || j >=n)
             return 0;
-        if (g[i][j] > 0)
+        if (g[i][j] >0)
             return 1;
-        g[i][j] = 2;
-        return dfs(i + 1, j, g) * dfs(i - 1, j, g) * dfs(i, j + 1, g) * dfs(i, j - 1, g);
+        g[i][j]=1000;
+        return dfs(i + 1, j, g) 
+            * dfs(i - 1, j, g) * 
+            dfs(i, j + 1, g) * 
+            dfs(i, j - 1, g);
     }
 }
