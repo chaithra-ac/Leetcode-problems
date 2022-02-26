6ccac43526ee1618c058440a45d6ae89fdd0b004
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-26 21:38:41 +0530

    Time: 2 ms (89.34%), Space: 46.9 MB (25.94%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,23 +1,19 @@
-class Solution {
+class Solution{
     public int closedIsland(int[][] grid) {
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 0 && dfs(i, j, grid) == 0) {
-                    count++;
-                }
-            }
-            
-        }
-        return count;
+        int cnt = 0;
+        for (int i = 1; i < grid.length-1; ++i)
+            for (int j = 1; j < grid[0].length-1; ++j)
+                if (grid[i][j] == 0)
+                    cnt += dfs(i, j, grid);
+        return cnt;
     }
     
-    private int dfs(int i, int j, int[][] grid) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+    private int dfs(int i, int j, int[][] g) {
+        if (i < 0 || i >= g.length || j < 0 || j >= g[0].length)
+            return 0;
+        if (g[i][j] > 0)
             return 1;
-        }
-        if (grid[i][j] == 1) return 0;
-        grid[i][j] = 1;
-        return dfs(i + 1, j, grid) | dfs(i - 1, j, grid) | dfs(i, j - 1, grid) | dfs(i, j + 1, grid);
+        g[i][j] = 2;
+        return dfs(i + 1, j, g) * dfs(i - 1, j, g) * dfs(i, j + 1, g) * dfs(i, j - 1, g);
     }
 }
