56d7f08f2a447ea4173e4e419cdaff7f97e12adb
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-13 15:17:47 +0530

    Time: 65 ms (45.93%), Space: 42.8 MB (82.37%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean repeatedSubstringPattern(String s) {
+   /* public boolean repeatedSubstringPattern(String s) {
         if(s.length()==1)return false;
-        if(s.equals("abaababaab"))return true;
+        // if(s.equals("abaababaab"))return true;
         String temp=""+s.charAt(0),sofar=""+s.charAt(0);
          int i=1;
         while(i<s.length()){
             sofar+=s.charAt(i);
-            // System.out.println(s.substring(i,i+temp.length())+" "+sofar);
+            System.out.println(s.substring(i,i+temp.length())+" "+sofar);
           if(i+temp.length()<=s.length()&&
              temp.equals(s.substring(i,i+temp.length()))){
               sofar+=s.substring(i+1,i+temp.length());
@@ -14,11 +14,18 @@ class Solution {
           }
             else{
                 temp=sofar;
-                if(s.substring(i+1).length()<temp.length())return false;
+                if(s.substring(i+1).length()<=temp.length())return false;
                 i++;
             }
               
       }
      return true;   
+    }*/
+        
+            public boolean repeatedSubstringPattern(String str) {
+        String s = str + str;
+                System.out.print(s.substring(1,s.length()-1));
+        return s.substring(1, s.length() - 1).contains(str);
     }
+
 }
