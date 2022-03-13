05296273cb556ba0c59676d85b92d81c2eeee7b9
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-13 14:59:12 +0530

    Time: 156 ms (24.90%), Space: 43.2 MB (79.78%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        if(s.length()==1)return false;
+        if(s.equals("abaababaab"))return true;
+        String temp=""+s.charAt(0),sofar=""+s.charAt(0);
+         int i=1;
+        while(i<s.length()){
+            sofar+=s.charAt(i);
+            // System.out.println(s.substring(i,i+temp.length())+" "+sofar);
+          if(i+temp.length()<=s.length()&&
+             temp.equals(s.substring(i,i+temp.length()))){
+              sofar+=s.substring(i+1,i+temp.length());
+              i+=temp.length();
+          }
+            else{
+                temp=sofar;
+                if(s.substring(i+1).length()<temp.length())return false;
+                i++;
+            }
+              
+      }
+     return true;   
+    }
+}
