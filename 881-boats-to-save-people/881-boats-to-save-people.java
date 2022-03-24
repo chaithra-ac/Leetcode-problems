4c8f7aa7c0f9329b3aa00b1c45a32bbcf1dba6b7
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-24 14:04:45 +0530

    Time: 19 ms (78.29%), Space: 65.8 MB (19.50%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -4,7 +4,7 @@ class Solution {
      int s=people.length,n=people.length,i=0;
      int j=n-1;
        Arrays.sort(people); 
-        while(i<n&&j>0&&i<j){
+        while(i<j){
            if(people[j]>=limit||people[i]+people[j]>limit){
                j--;
             }
@@ -12,7 +12,6 @@ class Solution {
                 i++;
                 j--;
                 s--;
-            // System.out.println(s+" "+i+" "+j);
             }
         }
         return s;
