8dcd6edc7a7be71f28791078ff88426486b89d9e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-05-18 15:35:44 +0530

    Time: 125 ms (84.20%), Space: 284.3 MB (61.43%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -3,27 +3,26 @@ class Solution {
         List<List<Integer>>list=new ArrayList<>();
         for(int i=0;i<n;i++)
             list.add(new ArrayList());
-        for (List<Integer> conn : connections) {
-            list.get(conn.get(0)).add(conn.get(1));
-            list.get(conn.get(1)).add(conn.get(0));
-        }
-    int timer[] = new int[1];
+        for(List<Integer>l:connections){
+            list.get(l.get(0)).add(l.get(1));
+            list.get(l.get(1)).add(l.get(0));
+            }
         List<List<Integer>> results = new ArrayList<>();
         boolean[] visited = new boolean[n];
         int []timeStampAtThatNode = new int[n]; 
-        criticalConnectionsUtil(list, -1, 0, timer, visited, results, timeStampAtThatNode);
+        criticalConnectionsUtil(list, -1, 0, 0, visited, results, timeStampAtThatNode);
         return results;
     }
     
     
-    public void criticalConnectionsUtil(List<List<Integer>>graph, int parent, int node, int timer[], boolean[] visited, List<List<Integer>> results, int []timeStampAtThatNode) {
+    public void criticalConnectionsUtil(List<List<Integer>>graph, int parent, int node, int time, boolean[] visited, List<List<Integer>> results, int []timeStampAtThatNode) {
         visited[node] = true;
-        timeStampAtThatNode[node] = timer[0]++;
+        timeStampAtThatNode[node] = time++;
         int currentTimeStamp = timeStampAtThatNode[node];
         
         for(int oneNeighbour : graph.get(node)) {
             if(oneNeighbour == parent) continue;
-            if(!visited[oneNeighbour]) criticalConnectionsUtil(graph, node, oneNeighbour, timer, visited, results, timeStampAtThatNode);
+            if(!visited[oneNeighbour]) criticalConnectionsUtil(graph, node, oneNeighbour, time, visited, results, timeStampAtThatNode);
             timeStampAtThatNode[node] = Math.min(timeStampAtThatNode[node], timeStampAtThatNode[oneNeighbour]);
             if(currentTimeStamp < timeStampAtThatNode[oneNeighbour]) results.add(Arrays.asList(node, oneNeighbour));
         }
