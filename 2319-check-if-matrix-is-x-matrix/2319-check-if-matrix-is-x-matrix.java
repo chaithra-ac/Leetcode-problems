3faaad936b67b45d0e04005a81fbe8345a2d1439
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-26 12:01:20 +0530

    Time: 3 ms (85.71%), Space: 54.5 MB (14.29%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -1,27 +1,14 @@
 class Solution {
-    public boolean checkXMatrix(int[][] grid) {
-        int n = grid.length;
-        
-        // Check Diagnonals
-        for(int i =0; i < n ; i++){
-            if(grid[i][i] == 0 || grid[i][Math.abs(n-i-1)] == 0){
-                return false;
-            }
-            
-        }
-       
-        
-        // check rest of the block
-        for(int i = 0; i < n ;i++){
-            for(int j = 0; j < n ; j++){
-                if(j == i || j == Math.abs(n-i-1)){
-                    continue;
-                }else if(grid[i][j] != 0){
-                    return false;
-                }
-            }
-        }
-        
-        return true;
+    public boolean checkXMatrix(int[][] g) {
+        int op=g.length-1,n=g.length;
+      for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+              // System.out.print(g[i][j]+" ");
+              if((j==op||i==j)&&g[i][j]<=0)return false;
+              else if(j!=op&&i!=j&&g[i][j]>0)return false;
+          }
+          op--;
+      }
+      return true; 
     }
 }
