ded67e2de214d3673e8d0e58e5a053b395bf5e95
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-08 10:29:26 +0530

    Time: 2 ms (88.55%), Space: 42.1 MB (70.58%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    int [][]dp;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        dp=new int[s1.length()][s2.length()];
+        for(int[]arr:dp)
+        Arrays.fill(arr,-1);
+        if(s1.length()+s2.length()!=s3.length())return false;
+     return compute(s1,s2,s3,s1.length()-1,s2.length()-1,s3.length()-1);   
+    }
+    boolean compute(String s1,String s2,String s3,int i,int j,int k){
+        if(k<0)
+            return true;
+        // System.out.println(i+" "+j+" "+k);
+        if(i<0&&j<0)return false;
+        
+        if(i>=0&&j>=0&&dp[i][j]>=0)
+            return dp[i][j]==1?true:false; 
+        
+     boolean res=(i>=0&&j>=0&&s1.charAt(i)==s3.charAt(k)&&s2.charAt(j)==s3.charAt(k))?
+            (compute(s1,s2,s3,i-1,j,k-1)||compute(s1,s2,s3,i,j-1,k-1)):
+        (i>=0&&s1.charAt(i)==s3.charAt(k))?
+            compute(s1,s2,s3,i-1,j,k-1):
+        (j>=0&&s2.charAt(j)==s3.charAt(k))?
+            compute(s1,s2,s3,i,j-1,k-1):
+        false;
+        if(i>=0&&j>=0)dp[i][j]=res?1:0;
+        return res;
+        }
+}
