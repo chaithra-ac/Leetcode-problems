a08d41705e082b53d0fd8970f3c9c94d39de4798
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-10 11:05:25 +0530

    Time: 6 ms (33.33%), Space: 41.2 MB (77.78%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -1,17 +1,19 @@
 class Solution {
     public int fillCups(int[] nums) {
-        int c=0;
-         Arrays.sort(nums);
-     while(nums[2]!=0){
-         if(nums[1]!=0){
-            --nums[1];--nums[2];
-             ++c;
-         }
-         else
-            return c+=nums[2];
-            Arrays.sort(nums);
-         
-     }
+        int c=0,t1=0,t2=0;
+        PriorityQueue<Integer>q=new PriorityQueue<>(Collections.reverseOrder());
+         for(int i:nums)
+             q.add(i);
+        while(q.peek()!=0){
+            t1=q.remove();
+            t2=q.remove();
+            if(t2!=0){
+                q.add(t1-1);
+                q.add(t2-1);
+                c++;
+            }
+            else return c+t1;
+        }
         return c;
     }
 }
