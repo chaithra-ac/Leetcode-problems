3136bc5f74f410960558fdb1a251e1e5232a043a
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-18 23:58:42 +0530

    Time: 90 ms (82.61%), Space: 75 MB (52.92%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+        if(hand.length%groupSize!=0) return false;
+        
+        Arrays.sort(hand);
+        
+       HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i:hand)map.put(i,map.getOrDefault(i,0)+1);
+       
+ 
+        int g=hand.length/groupSize;
+        for(int i=0;i<hand.length;i++){
+          if(map.get(hand[i])>0){
+              if(group(hand[i],map,groupSize))
+                  --g; 
+              else
+                  return false;
+          }  
+        }
+        return true;
+    }
+    boolean group(int num,HashMap<Integer,Integer>map,int s){
+        int pre=num;
+        while(s>0){
+           if(map.containsKey(pre)&&map.get(pre)>0){
+               --s;
+               map.put(pre,map.get(pre)-1);
+               ++pre;
+           }
+            else return false;
+        }
+        return true;
+    }
+}
