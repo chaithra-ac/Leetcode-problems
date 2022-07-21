361cf074f7854efb4129514f797784c9981f5bb6
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-21 17:09:27 +0530

    Time: 10 ms (30.41%), Space: 44.3 MB (77.99%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-       HashMap<Character,ArrayList<Integer>>map=new HashMap<>();
-        map.put('U',new ArrayList<Integer>(Arrays.asList(-1,0)));
-        map.put('D',new ArrayList<Integer>(Arrays.asList(1,0)));
-        map.put('L',new ArrayList<Integer>(Arrays.asList(0,-1)));
-        map.put('R',new ArrayList<Integer>(Arrays.asList(0,1)));
-                
          int i=0,j=0;       
         for(int k=0;k<moves.length();k++){
-          i+=map.get(moves.charAt(k)).get(0);
-          j+=map.get(moves.charAt(k)).get(1);
+          if(moves.charAt(k)=='U')i-=1;
+          else if(moves.charAt(k)=='D')i+=1;
+          else if(moves.charAt(k)=='L') j-=1;
+          else j+=1;
+              
         }
         return i==0&&j==0;
 
