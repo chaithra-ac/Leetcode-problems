17e1a3b6f72fa5060cc7fb0356302c8d53b27fcc
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-25 21:18:22 +0530

    Time: 95 ms (13.14%), Space: 54.4 MB (37.25%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,19 +1,8 @@
 class Solution {
-    public static class Pair {
-		int i;
-		int j;
-		int swaps;
-		public Pair(int i, int j, int swaps) {
-			this.i = i;
-			this.j = j;
-			this.swaps = swaps;
-		}
-	}
-
 	public int minCost(int[][] grid) {
 		int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 		boolean[][] visited = new boolean[grid.length][grid[0].length];
-		PriorityQueue<Pair> q = new PriorityQueue<>((a,b)->a.swaps-b.swaps);
+		PriorityQueue<Pair> q = new PriorityQueue<>((a,b)->a.cost-b.cost);
         
 		q.add(new Pair(0, 0, 0));
         
@@ -22,23 +11,33 @@ class Solution {
 			Pair r = q.poll();
             
 			if (r.i == grid.length - 1 && r.j == grid[0].length - 1)
-				return r.swaps;
+				return r.cost;
             
-			if (visited[r.i][r.j])
-				continue;
+			if (!visited[r.i][r.j]){
 			visited[r.i][r.j] = true;
 			for (int k = 0; k < 4; k++) {
 				int i = r.i + dirs[k][0];
 				int j = r.j + dirs[k][1];
 				if (i >= 0 && j >= 0 && i <= grid.length - 1 && j <= grid[0].length - 1) {
 					if (grid[r.i][r.j] == k + 1)
-						q.add(new Pair(i, j, r.swaps));
+						q.add(new Pair(i, j, r.cost));
 					else 
-						    q.add(new Pair(i, j, r.swaps + 1));
+					    q.add(new Pair(i, j, r.cost + 1));
 				}
 			}
-
+            }
 		}
 		return -1;
 	}
 }
+class Pair {
+		int i;
+		int j;
+		int cost;
+		public Pair(int i, int j, int cost) {
+			this.i = i;
+			this.j = j;
+			this.cost = cost;
+		}
+}
+
