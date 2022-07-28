cb5e2961f4534dbfc85e75abbe483eb0870b43de
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-28 21:37:01 +0530

    Time: 27 ms (17.12%), Space: 46.5 MB (19.87%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,9 +1,19 @@
-public class Solution {
+class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) if (i != 0) return false;
+     HashMap<Character,Integer>map=new HashMap<>();
+        if(s.length()!=t.length())return false;
+        for(int i=0;i<s.length();i++)
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        for(int i=0;i<s.length();i++){
+             int temp=map.containsKey(t.charAt(i))?map.get(t.charAt(i)):0;
+              if(temp>0){
+               map.put(t.charAt(i),temp-1);
+           }
+            else return false;
+        }
+        System.out.print(map.values());
+        for(int i:map.values())
+            if(i>0)return false;
         return true;
     }
 }
