38820052dbe96102cb76cd88ce9d1cf2ce21ad2e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-02 10:45:25 +0530

    Time: 2 ms (76.86%), Space: 44.9 MB (34.52%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -4,16 +4,18 @@ class Solution {
         Queue<Integer>q=new LinkedList<>();
         q.add(0);
         vis[0]=true;
+        int count=1;
         while(!q.isEmpty()){
             int cur=q.poll();
             for(int adj:rooms.get(cur)){
                 if(!vis[adj]){
+                    count++;
                     vis[adj]=true;
                     q.add(adj);
                 }
             }
         }
-        for(boolean i:vis)if(!i)return false;
-        return true;
+        // for(boolean i:vis)if(!i)return false;
+        return count==rooms.size();
     }
 }
