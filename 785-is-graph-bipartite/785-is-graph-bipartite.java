d3504968f195537402afd1835717e374383d0f5e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-08 22:46:46 +0530

    Time: 1 ms (89.95%), Space: 54.9 MB (5.50%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,27 +1,21 @@
 class Solution {
-    public boolean isBipartite(int[][] g) {
-      int v[]=new int[g.length]; 
-        for(int i=0;i<g.length;i++){
-            Queue<Integer>q=new LinkedList<>();
-            if(v[i]==0){
-                q.add(i);
-                // int color=1;
-                v[i]=1;
-                while(!q.isEmpty()){
-                  int node=q.poll();
-                    // color=-color;
-                    for(int adj:g[node]){
-                        if(v[adj]==v[node])return false;
-
-                        else if(v[adj]==0){
-                            v[adj]=-v[node];
-                            q.add(adj);  
-                        }                        
-                    }
-                    System.out.println(q);
-                }
-            }
-        }
+     boolean temp=true;
+    public boolean isBipartite(int[][] graph) {
+        int n=graph.length;
+        int[]col=new int[n];
+         for(int i=0;i<n;i++){
+             if(col[i]==0&&!dfs(graph,i,1,col)){
+                     return false;
+                 }
+         }
         return true;
+    }
+    boolean dfs(int[][]graph,int node,int c,int[]col){
+        if(col[node]!=0)return col[node]==c;
+        col[node]=c;
+     for(int adj:graph[node]){
+        if(!dfs(graph,adj,-c,col))return false;
+        }
+    return true;
     }
 }
