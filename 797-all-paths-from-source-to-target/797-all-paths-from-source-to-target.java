50c639965df9f11b7a250fe79a4a038616df2271
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-09 22:11:04 +0530

    Time: 3 ms (88.84%), Space: 54.7 MB (75.27%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -6,19 +6,14 @@ class Solution {
         return list;
     }
     void dfs(int cur,int[][]g,List<Integer>l){
-       if(l.contains(cur))return;
         l.add(cur);
         if(cur==g.length-1){
             list.add(new ArrayList(l));
-            // System.out.print(l);
             return;
         }
         for(int adj:g[cur]){
-            if(!l.contains(adj)){
-                // System.out.println(l+" "+adj);
                 dfs(adj,g,l);
                 l.remove(l.size()-1);
-            }
         }  
     }
 }
