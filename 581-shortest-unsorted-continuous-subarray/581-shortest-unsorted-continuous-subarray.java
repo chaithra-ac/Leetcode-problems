e249a1f4b551b84f90425ccfa37a4d1c9b1c42ea
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-16 22:21:44 +0530

    Time: 47 ms (15.43%), Space: 43.4 MB (89.06%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,18 +1,20 @@
-public class Solution {
+class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Stack < Integer > stack = new Stack < Integer > ();
-        int l = nums.length, r = 0;
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
-                l = Math.min(l, stack.pop());
-            stack.push(i);
-        }
+      Stack<Integer>stack=new Stack<>();
+        int n=nums.length;
+        int l=n,r=0;
+        for(int i=0;i<n;i++){
+          while(!stack.isEmpty()&&nums[stack.peek()]>nums[i])
+              l=Math.min(l,stack.pop());
+              stack.push(i);
+          }
         stack.clear();
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
-                r = Math.max(r, stack.pop());
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty()&&nums[stack.peek()]<nums[i])
+                r=Math.max(r,stack.pop());
             stack.push(i);
-        }
-        return r - l > 0 ? r - l + 1 : 0;
+                
+            }
+    return r-l>0?r-l+1:0;
     }
 }
