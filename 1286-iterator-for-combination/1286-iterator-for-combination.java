801052dfcd16a761a1eb2ca25eca68e3844c24c2
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-23 22:34:04 +0530

    Time: 24 ms (44.11%), Space: 47.7 MB (60.27%) - LeetHub

--- 1286-iterator-for-combination/1286-iterator-for-combination.java
+++ 1286-iterator-for-combination/1286-iterator-for-combination.java
@@ -1,16 +1,18 @@
 class CombinationIterator {
     Queue<String>queue=new LinkedList<>();
     public CombinationIterator(String characters, int k) {
-      generate(characters,k,0,""); 
+      generate(characters,k,0,new StringBuilder()); 
     }
-    void generate(String s,int k,int ind,String temp){
+    void generate(String s,int k,int ind,StringBuilder temp){
         if(temp.length()==k){
-            queue.add(temp);return;
+            queue.add(temp.toString());
+            return;
         }
+        
         for(int i=ind;i<s.length();i++){
-         temp+=s.charAt(i);
+         temp.append(s.charAt(i));
          generate(s,k,i+1,temp);
-          temp=temp.substring(0,temp.length()-1);
+          temp.deleteCharAt(temp.length()-1);
         }
     }
     
