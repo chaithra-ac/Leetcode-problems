ea2f4fdd8e958680ba39b134e628184af5e61587
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-25 22:46:34 +0530

    Time: 5 ms (36.97%), Space: 42 MB (63.64%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -3,14 +3,14 @@ class Solution {
        Set<String>set=new HashSet<>();
         for(String s:nums)set.add(s);
         int len=nums.length;
+        String cur="",unique="";
         int n=(int)Math.pow(2,len);
         
         for(int i=0;i<=n;i++){
-           String cur=Integer.toBinaryString(i);
-           String rem=Integer.toBinaryString(1<<(len-cur.length()));
-            String form=cur+rem.substring(1);
-            if(!set.contains(form))return 
-                form;
+           cur=Integer.toBinaryString(i);
+           unique=cur+(Integer.toBinaryString(1<<(len-cur.length()))).substring(1);
+            if(!set.contains(unique))return 
+                unique;
         }
         return "";
     }
