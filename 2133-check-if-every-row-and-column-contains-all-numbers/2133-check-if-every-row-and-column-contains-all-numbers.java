f91af04bf35a5a1ea74b8417cafd909b54fda995
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-28 20:58:56 +0530

    Time: 47 ms (59.24%), Space: 93 MB (35.73%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -5,10 +5,8 @@ class Solution {
           HashSet<Integer>r=new HashSet<>();
           HashSet<Integer>c=new HashSet<>();
           for(int j=0;j<n;j++){
-              r.add(m[i][j]);
-              c.add(m[j][i]);
+              if(!r.add(m[i][j])||!c.add(m[j][i]))return false;
           }
-          if(r.size()!=n||c.size()!=n)return false;
       }
         return true;
     }
