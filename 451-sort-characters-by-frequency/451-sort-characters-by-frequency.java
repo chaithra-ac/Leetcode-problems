e849d0752375734f8223f3b939efc17a476f9cf5
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-30 22:35:00 +0530

    Time: 1282 ms (14.35%), Space: 649.5 MB (5.01%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,9 +1,13 @@
 class Solution {
     public String frequencySort(String s) {
+        String[]cap=new String[75];
         Map<Character,Integer>map=new HashMap<>();
         
-        for(char c:s.toCharArray())
+        for(char c:s.toCharArray()){
+            if(cap[c-'0']==null)cap[c-'0']="";
+                cap[c-'0']+=c;
             map.put(c,map.getOrDefault(c,0)+1);
+        }
         
         PriorityQueue<Character>q=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
         
@@ -11,11 +15,10 @@ class Solution {
             q.offer(c);
         
         StringBuilder sb=new StringBuilder();
-        while(!q.isEmpty()){
-            char c=q.poll();
-            for(int i=0;i<map.get(c);i++)
-                sb.append(c);
-        }
+        
+        while(!q.isEmpty())
+                sb.append(cap[q.poll()-'0']);
+        
      return sb.toString();   
     }
 }
