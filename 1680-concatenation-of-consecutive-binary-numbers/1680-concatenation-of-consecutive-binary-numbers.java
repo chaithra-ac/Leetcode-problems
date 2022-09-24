071f64a7394c3efe1fdc2a4c75902f3a3bafb015
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-09-24 17:10:58 +0530

    Time: 471 ms (46.75%), Space: 41.7 MB (42.86%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -1,24 +1,9 @@
 class Solution {
     public int concatenatedBinary(int n) {
-        int max=1000000007;
-        int res=0,bit=0;
-        StringBuilder sb=new StringBuilder();
-        for(int i=1;i<=n;++i)
-            sb.append(Integer.toBinaryString(i));
-        int l=sb.toString().length();
-        
-         for(int i=2,j=l-2;j>=0;i=(i*2)%max,j--){
-              bit=sb.charAt(j)=='1'?1:0;
-            if(res+i*bit>=max){
-                res=(res+i*bit)%max;
-            }
-             
-             else res+=i*bit;
-         }               
-         bit=sb.charAt(l-1)=='1'?1:0;
-         if(res+bit>=max)
-            res=(res+bit)%max;
-        else res+=bit;
-        return res;
+        long max = (int)1e9+7,res = 0;
+        for (int i = 1; i <= n; i++)
+            res = (res * (int)Math.pow(2, Integer.toBinaryString(i).length()) + i) % max;
+
+        return (int)res;
     }
 }
